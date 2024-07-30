1d3576b5477d520ef29d671758460d923026e5ee
Swag-blips <coderblip@gmail.com>
2024-07-30 11:04:55 +0100

    blockchain data structure complete

--- dev/blockchain.js
+++ dev/blockchain.js
@@ -1,60 +1,77 @@
-const sha256 = require('sha256')
+const sha256 = require("sha256");
 
-function Blockchain () {
-    this.chain = []
-    this.pendingTransactions = []
+function Blockchain() {
+  this.chain = [];
+  this.pendingTransactions = [];
+
+  this.createNewBlock(100, "0", "0");
 }
 
-Blockchain.prototype.createNewBlock = function (nonce, previousBlockHash, hash) {
+Blockchain.prototype.createNewBlock = function (
+  nonce,
+  previousBlockHash,
+  hash
+) {
   const newBlock = {
     index: this.chain.length + 1,
     timestamp: Date.now(),
     transactions: this.pendingTransactions,
     nonce: nonce,
     hash: hash,
-    previousBlockHash: previousBlockHash
-  }
+    previousBlockHash: previousBlockHash,
+  };
 
-  this.pendingTransactions = []
-  this.chain.push(newBlock)
+  this.pendingTransactions = [];
+  this.chain.push(newBlock);
 
-  return newBlock
-}
+  return newBlock;
+};
 
-Blockchain.prototype.getLastBlock = function (){
-    return this.chain[this.chain.length - 1]
-}
+Blockchain.prototype.getLastBlock = function () {
+  return this.chain[this.chain.length - 1];
+};
 
-Blockchain.prototype.createNewTransaction = function (amount, sender, recipient){
-    const newTransaction = {
-        amount: amount,
-        sender: sender,
-        recipient: recipient,
-    }
+Blockchain.prototype.createNewTransaction = function (
+  amount,
+  sender,
+  recipient
+) {
+  const newTransaction = {
+    amount: amount,
+    sender: sender,
+    recipient: recipient,
+  };
 
-    this.pendingTransactions.push(newTransaction)
+  this.pendingTransactions.push(newTransaction);
 
-    return this.getLastBlock()['index'] + 1;
+  return this.getLastBlock()["index"] + 1;
+};
 
-}
+Blockchain.prototype.hashBlock = function (
+  previousBlockHash,
+  currentBlockData,
+  nonce
+) {
+  const dataAsString =
+    previousBlockHash + nonce.toString() + JSON.stringify(currentBlockData);
+  const hash = sha256(dataAsString);
 
-Blockchain.prototype.hashBlock = function(previousBlockHash, currentBlockData, nonce){
-   const dataAsString = previousBlockHash+nonce.toString()+JSON.stringify(currentBlockData)
-   const hash = sha256(dataAsString)
+  return hash;
+};
 
-   return hash
-}
-
-
-Blockchain.prototype.proofOfWork = function(previousBlockHash, currentBlockData){
+Blockchain.prototype.proofOfWork = function (
+  previousBlockHash,
+  currentBlockData
+) {
   let nonce = 0;
   let hash = this.hashBlock(previousBlockHash, currentBlockData, nonce);
-  while (hash.substring(0,4) != "0000"){
+  while (hash.substring(0, 4) != "0000") {
     nonce++;
-    hash = this.hashBlock(previousBlockHash, currentBlockData, nonce)
+    hash = this.hashBlock(previousBlockHash, currentBlockData, nonce);
+    console.log(hash);
   }
 
   return nonce;
-}
+};
 
-module.exports = Blockchain
+module.exports = Blockchain;
--- dev/test.js
+++ dev/test.js
@@ -1,27 +1,5 @@
-const Blockchain = require('./blockchain')
-
+const Blockchain = require("./blockchain");
 
 const bitcoin = new Blockchain();
 
-const previousBlockHash = 'OINA3DFEFRH234GH2'
-const currentBlockData = [
-    {
-        "amount": 50,
-        "sender": "A1B2C3D4E5F6G7H8",
-        "recepient": "I9J0K1L2M3N4O5P6"
-    },
-    {
-        "amount": 20,
-        "sender": "QR8S9T0U1V2W3X4",
-        "recepient": "Y5Z6A7B8C9D0E1F2"
-    },
-    {
-        "amount": 35,
-        "sender": "G3H4I5J6K7L8M9N0",
-        "recepient": "O1P2Q3R4S5T6U7V8"
-    },
-]
-
-const nonce = 100
-
-console.log(bitcoin.hashBlock(previousBlockHash, currentBlockData, nonce))
+console.log(bitcoin);
