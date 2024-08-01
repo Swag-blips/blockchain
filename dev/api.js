5f0010810fc41a17a6b5a77936c8743fe832186f
Swag-blips <coderblip@gmail.com>
2024-08-01 21:01:29 +0100

    few changes

--- dev/api.js
+++ dev/api.js
@@ -1,18 +1,30 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const Blockchain = require("./blockchain");
 const app = express();
+
+const bitcoin = new Blockchain();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.get("/blockchain", function (req, res) {});
-
-app.post("/transaction", function (req, res) {
-  console.log(req.body);
-  res.send(`The amount of the transaction ${req.body.amount} bitcoin`);
+app.get("/blockchain", function (req, res) {
+  res.send(bitcoin);
 });
 
-app.get("/mine", function (req, res) {});
+app.post("/transaction", function (req, res) {
+  const blockIndex = bitcoin.createNewTransaction(
+    req.body.amount,
+    req.body.sender,
+    req.body.recipient
+  );
+
+  res.json({ note: `Transaction will be added in block ${blockIndex}.` });
+});
+
+app.get("/mine", function (req, res) {
+  
+});
 app.listen(3000, () => {
   console.log(`Server is listening on port 3000`);
 });
